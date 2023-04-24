27a508aee5d16206aadd3b28796a4c9b3fb4457e
zhaoxuepeng <1492548812@qq.com>
2023-04-24 12:32:31 +0800

    丝袜哥配置

--- index.js
+++ index.js
@@ -16,33 +16,9 @@ const joi = require('joi')
 
 //app
 const app = express()
-const expressSwagger = require('express-swagger-generator')(app)
-let options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: 'localhost:3000',
-        basePath: '/',
-        produces: [
-            "application/json",
-        ],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: __dirname, //app absolute path
-    files: ['./routes/**/*.js'] //Path to the API handle folder
-};
-expressSwagger(options)
+// 使用swagger API 文档
+const swaggerInstall = require('./utils/swagger')
+swaggerInstall(app)
 
 
 
--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,40 @@
 
 const express = require('express')
-const expressJoi = require('@escook/express-joi')
 
+const expressJoi = require('@escook/express-joi')
 const router = express.Router()
 //处理函数引入
 const user = require('../routes_handler/user')
-
-
 //joi
 const user_schema = require('../schema/user_schema')
-
 /**
- * 啊实打实大苏打
- * @route post /login/register
- * @group login - 登陆注册
- * @param {body} email.body.required - username or email - eg: user@domain
- * @returns {object} 200 - An array of user info
- * @returns {Error}  default - Unexpected error
+@swagger
+ * /user/register:
+ *    post:
+ *      summary: 注册用户
+ *      tags: [登陆注册]
+ *      description: 注册用户
+ *      parameters:
+ *        - in: body
+ *          name: body
+ *          schema:
+ *            type: object
+ *            required: [email,nickName,password]
+ *            description: user's credential
+ *            properties:
+ *              email:
+ *                type: string
+ *              nickName:
+ *                type: string
+ *              password:
+ *                type: string
+ *                minLength: 6
+ *      responses:
+ *        200:
+ *          description: OK
  */
 router.post('/register', expressJoi(user_schema.reg_schema), user.register)
-/**
- * 啊实打实大苏打
- * @route post /login/login
- * @group login
- * @param {string} email.body.required - username or email - eg: user@domain
- * @returns {object} 200 - An array of user info
- * @returns {Error}  default - Unexpected error
- */
+
 router.post('/login', expressJoi(user_schema.login_schema), user.login)
 
-//暴露
 module.exports = router
